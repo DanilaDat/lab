c5161ca96da4c03d746bed0b0ca49b0a5d8312cb
DanilaDat <71312561+DanilaDat@users.noreply.github.com>
2020-09-15 10:30:11 +0300

    Add files via upload

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+
+bool Solve(double a, double b, double& x)
+{
+	b = b * (-1);
+	x = b / a;
+	return true;
+}
+
+
+int main()
+{
+	int a, b;
+	cout << "input a,b:\n";
+	cin >> a >> b;
+	double x = 0;
+	bool temp = Solve(a, b, x);
+	cout << setw(10) << setfill('+');
+	cout << x << endl;
+	system("pause");
+}
+
+
+
